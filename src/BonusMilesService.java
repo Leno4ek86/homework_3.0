45aa898a7161fec53ee8762badb5b98161d65ed8
Leno4ek86 <102864231+Leno4ek86@users.noreply.github.com>
2022-04-29 11:48:27 +0300

    добавлен код расчета милей

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+
+        int bonus = cost / 20;
+
+        // System.out.println (cost / mile + "начислено милей");
+        return bonus;
+    }
+}
